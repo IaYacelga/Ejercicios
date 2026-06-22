ad30ec8915dae06341341b6943d606e6a576aadd
Ivan Andres <ivanyacelga001@gmail.com>
2026-06-22 00:46:01 -0500

    Prueba main con git

---
+++ Programacion en C++/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hola Mundo!" << endl;
+
+    // Variables
+    int edad = 20;
+    string nombre = "Juan";
+
+    cout << "Nombre: " << nombre << endl;
+    cout << "Edad: " << edad << endl;
+
+    return 0;
+}
