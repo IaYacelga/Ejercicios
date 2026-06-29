934a2daa5f1735b0e7f2a6a6c052bd2edb5c8e15
Ivan Andres <ivanyacelga001@gmail.com>
2026-06-28 23:24:47 -0500

    PROBAR EN LA COMPUTAODRA 2

--- Programacion en C++/archivos.cpp
+++ Programacion en C++/archivos.cpp
@@ -5,8 +5,18 @@ using namespace std;
 
 
 int main(void){
+
+
+    ofstream nombres("nombres.txt"); 
+
+    if(!nombres.is_open()){
+        cout << "Error al abrir el archiv" << endl; 
+        return 1; 
+    }
     
- 
+
+   nombres << "IVAN ANDRES YACELGA MALITAXI" << endl; 
+   nombres.close(); 
 
 
     return 0;
